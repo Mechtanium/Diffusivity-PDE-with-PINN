1381b21a3d832c86400771b12efa3f18567bbcd2
Mechtanium <a.sapiens.view@gmail.com>
2023-09-18 17:48:19 +0100

    Small change

--- main.py
+++ main.py
@@ -3,9 +3,31 @@ import tensorflow as tf
 import numpy as np
 
 
-def sim_data():
-    return {}
+def sim_data(count=100):
+    return {
+        "µ": [],
+        "c": [],
+        "∅": [],
+        "∂P/∂x": [],
+        "∂P/∂t": []
+    }
 
 
-data = sim_data()
-print(data)
+data = pd.DataFrame(sim_data(10000))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=5),
+    tf.keras.layers.Dense(units=10000),
+    tf.keras.layers.Dense(units=10000),
+    tf.keras.layers.Dense(units=10000),
+    tf.keras.layers.Dense(units=10000),
+    tf.keras.layers.Dense(units=10000),
+    tf.keras.layers.Dense(units=10000),
+    tf.keras.layers.Dense(units=10000),
+    tf.keras.layers.Dense(units=10000),
+    tf.keras.layers.Dense(units=1)
+])
+y = 0
+predicted = 0
+
+mse_loss = tf.keras.losses.MSE(data["∂P/∂x"], data[""])
